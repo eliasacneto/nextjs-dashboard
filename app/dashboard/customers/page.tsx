236736409a7cc030428339f599efb1749e235ad7
Elias Neto <netoeac.dev@gmail.com>
2023-12-23 09:07:35 -0300

    customers page added

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,20 @@
+// Importe os módulos corretos de acordo com a estrutura do seu projeto
+import CustomersTable from '@/app/ui/customers/table';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+
 export default function Page() {
-  return <p>Customers Page</p>;
+  // Simule uma lista de clientes para passar como propriedade para o componente CustomersTable
+  const customers: FormattedCustomersTable[] = [
+    { id: 1, name: 'Customer 1', email: 'customer1@example.com', /* ... outras propriedades ... */ },
+    { id: 2, name: 'Customer 2', email: 'customer2@example.com', /* ... outras propriedades ... */ },
+    // ... mais clientes
+  ];
+
+  return (
+    <div>
+      {/* Renderize o componente CustomersTable e passe a propriedade customers */}
+      <CustomersTable customers={customers} />
+      {/* Outro conteúdo da sua página... */}
+    </div>
+  );
 }
